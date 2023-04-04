b902491437a4d19af960d5dfb27f5896e457241d
maxefimov <efimov.academy.top@gmail.com>
2023-04-04 19:47:48 +0300

    Добавьте файлы проекта.

---
+++ OopPointersProject/OopPointersProject.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N;
+
+
+struct Person
+{
+    char name[50];
+    int age;
+    Person* parent;
+};
+
+
+int Sum(int a, int b)
+{
+    N = 100;
+    return a + b;
+}
+
+int Mult(int a, int b)
+{
+    N = 200;
+    return a * b;
+}
+
+int Calc(int a, int b, int (*oper)(int, int))
+{
+    return oper(a, b);
+}
+
+int Counter()
+{
+    static int count = 0;
+    return ++count;
+}
+
+
+int main()
+{
+    int (*fptr)(int, int);
+    
+    fptr = Sum;
+    cout << fptr(10, 20) << "\n";
+
+    fptr = Mult;
+    cout << fptr(10, 20) << "\n";
+
+    cout << Calc(10, 20, Sum) << "\n";
+    cout << Calc(10, 20, Mult) << "\n";
+
+
+    int* ptr = new int;
+    *ptr = 30;
+
+
+    Person personStack;
+    personStack.age = 30;
+
+
+
+    Person* person = new Person;
+    person->age = 30;
+
+    delete person;
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << Counter() << "\n";
+    }
+
+}
